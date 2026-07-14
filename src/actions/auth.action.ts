1f7fa5a8e93bb16f75fc5f2bd301742ac5f806b8
Parvez141300 <parvezhossain744471@gmail.com>
2026-07-14 17:34:25 +0600

    imporve the httpClient and httpServer

--- src/actions/auth.action.ts
+++ src/actions/auth.action.ts
@@ -1,62 +1,63 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-"use server";
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// "use server";
 
-import { cookieUtils } from "@/lib/cookieUtilts";
-import { envVars } from "@/lib/env";
-import { headers } from "next/headers";
+// import { cookieUtils } from "@/lib/cookieUtilts";
+// import { envVars } from "@/lib/env";
+// import { headers } from "next/headers";
 
-export const getNewTokenWithRefreshToken = async (refreshToken: string, betterAuthSessionToken: string) => {
-    try {
-        const requestHeaders = await headers();
+// export const getNewTokenWithRefreshToken = async (refreshToken: string, betterAuthSessionToken: string) => {
+//     try {
+//         const requestHeaders = await headers();
 
-        let response;
+//         let response;
 
-        response = await fetch(`${envVars.BACKEND_URL}/api/v1/refresh-token`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Cookie: `refreshToken=${refreshToken}; better-auth.session_token=${betterAuthSessionToken}`
-            }
-        });
+//         response = await fetch(`${envVars.BACKEND_URL}/api/v1/refresh-token`, {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Cookie: `refreshToken=${refreshToken}; better-auth.session_token=${betterAuthSessionToken}`
+//             }
+//         });
 
-        if (requestHeaders.get("X-Refresh-Token-Refresh")) {
-            response = await fetch(`${envVars.BACKEND_URL}/api/v1/refresh-token`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Cookie: `refreshToken=${refreshToken}; better-auth.session_token=${betterAuthSessionToken}`
-                }
-            });
-        }
+//         if (requestHeaders.get("X-Refresh-Token-Refresh")) {
+//             response = await fetch(`${envVars.BACKEND_URL}/api/v1/refresh-token`, {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     Cookie: `refreshToken=${refreshToken}; better-auth.session_token=${betterAuthSessionToken}`
+//                 }
+//             });
+//         }
 
-        if (response.ok) {
+//         if (response.ok) {
 
-            const data = await response.json();
+//             const data = await response.json();
 
-            const { accessToken, refreshToken: newRefreshToken, betterAuthSessionToken: newBetterAuthSessionToken } = data;
+//             const { accessToken, refreshToken: newRefreshToken, betterAuthSessionToken: newBetterAuthSessionToken } = data;
 
-            // Set the new tokens in cookies
+//             // Set the new tokens in cookies
 
-            if (accessToken) {
-                // set access token for 1 day
-                await cookieUtils.setCookie("accessToken", accessToken, 60 * 60 * 24);
-            }
-            if (newRefreshToken) {
-                // set refresh token for 7 days
-                await cookieUtils.setCookie("refreshToken", newRefreshToken, 60 * 60 * 24 * 7);
-            }
-            if (newBetterAuthSessionToken) {
-                // set better auth session token for 1 day
-                await cookieUtils.setCookie("better-auth.session_token", newBetterAuthSessionToken, 60 * 60 * 24);
-            }
+//             if (accessToken) {
+//                 // set access token for 1 day
+//                 await cookieUtils.setCookie("accessToken", accessToken, 60 * 60 * 24);
+//             }
+//             if (newRefreshToken) {
+//                 // set refresh token for 7 days
+//                 await cookieUtils.setCookie("refreshToken", newRefreshToken, 60 * 60 * 24 * 7);
+//             }
+//             if (newBetterAuthSessionToken) {
+//                 // set better auth session token for 1 day
+//                 await cookieUtils.setCookie("better-auth.session_token", newBetterAuthSessionToken, 60 * 60 * 24);
+//             }
 
-            return true;
-        }
-        else {
-            return false;
-        }
-    } catch (error: any) {
-        console.error("Error refreshing token:", error.message);
-        return false;
-    }
-};
+//             return true;
+//         }
+//         else {
+//             return false;
+//         }
+//     } catch (error: any) {
+//         console.error("Error refreshing token:", error.message);
+//         return false;
+//     }
+// };
+
--- src/lib/axios/httpClient.ts
+++ src/lib/axios/httpClient.ts
@@ -1,107 +1,107 @@
 
-import { cookieUtils } from "../cookieUtilts"
-import { tokenUtils } from "../tokenUtils";
-import { getNewTokenWithRefreshToken } from "@/actions/auth.action";
-import axios from "axios";
-import { envVars } from "../env";
+// import { cookieUtils } from "../cookieUtilts"
+// import { tokenUtils } from "../tokenUtils";
+// import { getNewTokenWithRefreshToken } from "@/actions/auth.action";
+// import axios from "axios";
+// import { envVars } from "../env";
 
 
-export const axiosInstance = async () => {
-    const accessToken = await cookieUtils.getCookie("accessToken");
-    const refreshToken = await cookieUtils.getCookie("refreshToken");
-    const betterAuthSessionToken = await cookieUtils.getCookie("better-auth.session_token");
+// export const axiosInstance = async () => {
+//     const accessToken = await cookieUtils.getCookie("accessToken");
+//     const refreshToken = await cookieUtils.getCookie("refreshToken");
+//     const betterAuthSessionToken = await cookieUtils.getCookie("better-auth.session_token");
 
-    if (accessToken && refreshToken && betterAuthSessionToken) {
-        const accessTokenExpired = await tokenUtils.isTokenExpired(accessToken as string);
-        const refreshTokenExpired = await tokenUtils.isTokenExpired(refreshToken as string);
-        if (refreshTokenExpired) {
-            // logout
-            throw new Error("Refresh token expired. Please login again.");
-        }
-        if (accessTokenExpired) {
-            await getNewTokenWithRefreshToken(refreshToken as string, betterAuthSessionToken as string);
-        }
-    }
+//     if (accessToken && refreshToken && betterAuthSessionToken) {
+//         const accessTokenExpired = await tokenUtils.isTokenExpired(accessToken as string);
+//         const refreshTokenExpired = await tokenUtils.isTokenExpired(refreshToken as string);
+//         if (refreshTokenExpired) {
+//             // logout
+//             throw new Error("Refresh token expired. Please login again.");
+//         }
+//         if (accessTokenExpired) {
+//             await getNewTokenWithRefreshToken(refreshToken as string, betterAuthSessionToken as string);
+//         }
+//     }
 
-    const instance = axios.create({
-        baseURL: envVars.NEXT_PUBLIC_BACKEND_URL,
-        timeout: 5000, // 5 seconds if no response then error
-        withCredentials: true,
-    });
+//     const instance = axios.create({
+//         baseURL: envVars.NEXT_PUBLIC_BACKEND_URL,
+//         timeout: 5000, // 5 seconds if no response then error
+//         withCredentials: true,
+//     });
 
-    return instance;
-}
-
-export interface IApiRequestOptions {
-    params: Record<string, unknown>;
-    headers: Record<string, string>;
-}
-
-export interface IApiResponse<TData> {
-    success: boolean;
-    message: string;
-    data: TData;
-}
-
-const httpGet = async <TData>(url: string, options?: IApiRequestOptions) => {
-    const instance = await axiosInstance();
-
-    const response = await instance.get<IApiResponse<TData>>(url, {
-        params: options?.params,
-        headers: options?.headers,
-    });
-
-    return response.data;
-}
-
-const httpPost = async <TData>(url: string, data?: unknown, options?: IApiRequestOptions) => {
-    const instance = await axiosInstance();
-
-    const response = await instance.post<IApiResponse<TData>>(url, data, {
-        params: options?.params,
-        headers: options?.headers,
-    });
-
-    return response.data;
-}
-
-const httpPatch = async <TData>(url: string, data?: unknown, options?: IApiRequestOptions) => {
-    const instance = await axiosInstance();
-
-    const response = await instance.patch<IApiResponse<TData>>(url, data, {
-        params: options?.params,
-        headers: options?.headers,
-    });
-
-    return response.data;
-}
-
-const httpPut = async <TData>(url: string, data?: unknown, options?: IApiRequestOptions) => {
-    const instance = await axiosInstance();
-
-    const response = await instance.put<IApiResponse<TData>>(url, data, {
-        params: options?.params,
-        headers: options?.headers,
-    });
-
-    return response.data;
-}
-
-const httpDelete = async <TData>(url: string, options?: IApiRequestOptions) => {
-    const instance = await axiosInstance();
-
-    const response = await instance.delete<IApiResponse<TData>>(url, {
-        params: options?.params,
-        headers: options?.headers,
-    });
-
-    return response.data;
-}
-
-// export const httpClient = {
-//     get: httpGet,
-//     post: httpPost,
-//     patch: httpPatch,
-//     put: httpPut,
-//     delete: httpDelete,
+//     return instance;
 // }
+
+// export interface IApiRequestOptions {
+//     params: Record<string, unknown>;
+//     headers: Record<string, string>;
+// }
+
+// export interface IApiResponse<TData> {
+//     success: boolean;
+//     message: string;
+//     data: TData;
+// }
+
+// const httpGet = async <TData>(url: string, options?: IApiRequestOptions) => {
+//     const instance = await axiosInstance();
+
+//     const response = await instance.get<IApiResponse<TData>>(url, {
+//         params: options?.params,
+//         headers: options?.headers,
+//     });
+
+//     return response.data;
+// }
+
+// const httpPost = async <TData>(url: string, data?: unknown, options?: IApiRequestOptions) => {
+//     const instance = await axiosInstance();
+
+//     const response = await instance.post<IApiResponse<TData>>(url, data, {
+//         params: options?.params,
+//         headers: options?.headers,
+//     });
+
+//     return response.data;
+// }
+
+// const httpPatch = async <TData>(url: string, data?: unknown, options?: IApiRequestOptions) => {
+//     const instance = await axiosInstance();
+
+//     const response = await instance.patch<IApiResponse<TData>>(url, data, {
+//         params: options?.params,
+//         headers: options?.headers,
+//     });
+
+//     return response.data;
+// }
+
+// const httpPut = async <TData>(url: string, data?: unknown, options?: IApiRequestOptions) => {
+//     const instance = await axiosInstance();
+
+//     const response = await instance.put<IApiResponse<TData>>(url, data, {
+//         params: options?.params,
+//         headers: options?.headers,
+//     });
+
+//     return response.data;
+// }
+
+// const httpDelete = async <TData>(url: string, options?: IApiRequestOptions) => {
+//     const instance = await axiosInstance();
+
+//     const response = await instance.delete<IApiResponse<TData>>(url, {
+//         params: options?.params,
+//         headers: options?.headers,
+//     });
+
+//     return response.data;
+// }
+
+// // export const httpClient = {
+// //     get: httpGet,
+// //     post: httpPost,
+// //     patch: httpPatch,
+// //     put: httpPut,
+// //     delete: httpDelete,
+// // }
